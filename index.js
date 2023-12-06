4b1c3b062c3c48da8bca229bdd7e266bada38f37
xiyu <1943840816@qq.com>
2023-12-07 02:13:06 +0800

    init www

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "wasm-fa";
 
-wasm.greet();
+var input = document.getElementById('myInput');
+input.addEventListener('input', function () {
+    this.value = this.value.replace(/[^01*()+]/g, '');
+});
+input.addEventListener('keypress',  (e) => {
+    if (e.key === 'Enter') {
+        handleClick();
+    }
+});
+
+let button = document.querySelector("button");
+button.addEventListener("click", () => {
+    handleClick();
+});
+
+function handleClick() {
+    var input = document.getElementById('myInput');
+    var value = input.value;
+    value = value.replace(/\+/g,'\|');
+    var result = wasm.get_ans(value);
+    let [ans, rg] = result.split('@');
+
+    var output = document.getElementById('output');
+    var outputrg = document.getElementById('rg');
+
+    output.textContent = ans;
+    outputrg.textContent = rg;
+    console.log(result);
+}
